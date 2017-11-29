0abb28fa2d164ec14428ec7dcae324f949c11801
yjpal <yl21ly@qq.com>
2017-11-29 15:35:09 +0800

    fix

--- Example/Pods/Target Support Files/LTAlertView/LTAlertView-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "LTAlertView.h"
-
-FOUNDATION_EXPORT double LTAlertViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char LTAlertViewVersionString[];
-
--- Example/Pods/Target Support Files/Pods-LTAlertView_Tests/Pods-LTAlertView_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_LTAlertView_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LTAlertView_TestsVersionString[];
-
